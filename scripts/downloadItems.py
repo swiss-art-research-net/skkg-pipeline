1529de50f02a73fc863c165dca5512ec0aff4a56
Florian Kräutli <florian.kraeutli@uzh.ch>
2023-06-14 13:03:24 +0200

    store mapping between indices and uuid

--- scripts/downloadItems.py
+++ scripts/downloadItems.py
@@ -1,14 +1,23 @@
 import argparse
+import csv
 import sys
 from lxml import etree
-from os.path import join
+from os.path import join, exists
 from tqdm import tqdm
 
 from lib.MuseumPlusConnector import MPWrapper
 
-def downloadItems(*, host, username, password, outputFolder, filenamePrefix = 'item-', limit = None, offset = None):          
+def downloadItems(*, host, username, password, outputFolder, uuidMapCsv, filenamePrefix = 'item-', limit = None, offset = None):          
     client = MPWrapper(url=host, username=username, password=password)
 
+    # Log the downloaded files
+    log = {
+        'downloaded': [],
+        'existing': []
+    }
+
+    uuidMap = getUuidMap(uuidMapCsv);
+    
     # Get the number of objects
     numObjects = client.getNumberOfObjects()
 
@@ -18,12 +27,36 @@ def downloadItems(*, host, username, password, outputFolder, filenamePrefix = 'i
         offset = 0
 
     for i in tqdm(range(offset, offset+limit)):
-        print(i)
-        # item = client.getObjectByOffset(i)
-        # id = item.find('.//{http://www.zetcom.com/ria/ws/module}moduleItem').get('id')
-        # filename = join(outputFolder, filenamePrefix + id + ".xml")
-        # with open(filename, 'wb') as f:
-        #     f.write(etree.tostring(item, pretty_print=True))
+        filename = join(outputFolder, filenamePrefix + str(i) + ".xml")
+        # Check if the file already exists
+        if not exists(filename):
+            item = client.getObjectByOffset(i)
+            uuid = item.find('.//{http://www.zetcom.com/ria/ws/module}moduleItem').get('uuid')
+            with open(filename, 'wb') as f:
+                f.write(etree.tostring(item, pretty_print=True))
+                uuidMap[i] = uuid
+                log['downloaded'].append(filename)
+        else:
+            log['existing'].append(filename)
+    
+    writeUuidMap(uuidMap, uuidMapCsv)
+
+    print(f"Downloaded {len(log['downloaded'])} items.")
+    print(f"Skipped {len(log['existing'])} items that already existed.")
+
+def getUuidMap(filename):
+    uuidMap = {}
+    if exists(filename):
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            uuidMap = {rows[0]:rows[1] for rows in reader}
+    return uuidMap
+
+def writeUuidMap(uuidMap, filename):
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        for key, value in uuidMap.items():
+            writer.writerow([key, value])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -33,6 +66,7 @@ if __name__ == "__main__":
     parser.add_argument('--username', required= True, help='Username to use for authentication')
     parser.add_argument('--password', required= True, help='Password to use for authentication')
     parser.add_argument('--outputFolder', required= True, help='Folder to save the XML files to')
+    parser.add_argument('--uuidMap', required= True, help='Path to a CSV file used to store corresponding indices and UUIDs')
     parser.add_argument('--filenamePrefix', required= False, help='Prefix to use for the filenames of the XML files. Defaults to "item-"')
     parser.add_argument('--limit', required= False, help='Limit the number of items to download')
     parser.add_argument('--offset', required= False, help='Offset to start downloading items from')
@@ -44,4 +78,4 @@ if __name__ == "__main__":
     if args.offset:
         args.offset = int(args.offset)
 
-    downloadItems(host=args.url, username=args.username, password=args.password, outputFolder=args.outputFolder, filenamePrefix=args.filenamePrefix or 'item-', limit=args.limit, offset=args.offset)
+    downloadItems(host=args.url, username=args.username, password=args.password, uuidMapCsv=args.uuidMap, outputFolder=args.outputFolder, filenamePrefix=args.filenamePrefix or 'item-', limit=args.limit, offset=args.offset)
