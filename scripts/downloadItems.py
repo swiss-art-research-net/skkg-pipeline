7e9e9532418dda025ee02aa2be3e04c947ec9eec
Florian Kräutli <florian.kraeutli@uzh.ch>
2023-07-06 12:30:51 +0200

    move metadata routines to separate class

--- scripts/downloadItems.py
+++ scripts/downloadItems.py
@@ -1,64 +1,11 @@
 import argparse
-import json
 from datetime import datetime
-from lxml import etree
 from os import listdir, remove as removeFile
 from os.path import join, exists, isfile
 from tqdm import tqdm
 
 from lib.MuseumPlusConnector import MPWrapper
-
-
-class ItemMetadata:
-    METADATA_FILENAME = 'metadata.json'
-
-    directory = None
-
-    def __init__(self, directory):
-        self.directory = directory
-
-    def getLastUpdatedDate(self):
-        # Look for metadata file in the output folder
-        metadataFilename = join(self.directory, self.METADATA_FILENAME)
-        if exists(metadataFilename):
-            # Read the metadata file
-            with open(metadataFilename, 'r') as f:
-                metadata = json.load(f)
-            # Get last updated date from metadata
-            lastUpdated = metadata['lastUpdated']
-        else:
-            # Get the last updated date from the existing files
-            lastUpdated  = self.getLastUpdatedFromItemFiles(self.directory)
-            if lastUpdated:
-                # Save the last updated date in a metadata file
-                with open(metadataFilename, 'w') as f:
-                    json.dump({'lastUpdated': lastUpdated}, f)
-        return lastUpdated
-    
-    def getLastUpdatedFromItemFiles(self):
-        # Read all XML files in the input folder
-        files = [f for f in listdir(self.directory) if isfile(join(self.directory, f)) and f.endswith('.xml')]
-        
-        # If no files exist yet, return None
-        if len(files) == 0:
-            return None
-
-        # Set lastUpdated to a Date object with the lowest possible value
-        lastUpdated = datetime.min
-
-        for file in tqdm(files):
-            tree = etree.parse(join(self.directory, file))
-            lastUpdatedString= tree.find('.//{http://www.zetcom.com/ria/ws/module}systemField[@name="__lastModified"]/{http://www.zetcom.com/ria/ws/module}value').text
-            lastUpdatedItem = datetime.strptime(lastUpdatedString, '%Y-%m-%d %H:%M:%S.%f')
-            if lastUpdatedItem > lastUpdated:
-                lastUpdated = lastUpdatedItem
-        return lastUpdated.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
-
-    def setLastUpdated(self, lastUpdated):
-        metadataFilename = join(self.directory, self.METADATA_FILENAME)
-        with open(metadataFilename, 'w') as f:
-            json.dump({'lastUpdated': lastUpdated.strftime('%Y-%m-%dT%H:%M:%S.%f')}, f)
-
+from lib.Metadata import ItemMetadata
 
 def downloadItems(*, host, username, password, module, outputFolder, tempFolder, filenamePrefix = 'item-', limit = None, offset = None):          
     client = MPWrapper(url=host, username=username, password=password)
---
+++ scripts/lib/Metadata.py
@@ -0,0 +1,53 @@
+import json
+from os.path import join, exists, isfile
+from lxml import etree
+
+class ItemMetadata:
+    METADATA_FILENAME = 'metadata.json'
+
+    directory = None
+
+    def __init__(self, directory):
+        self.directory = directory
+
+    def getLastUpdatedDate(self):
+        # Look for metadata file in the output folder
+        metadataFilename = join(self.directory, self.METADATA_FILENAME)
+        if exists(metadataFilename):
+            # Read the metadata file
+            with open(metadataFilename, 'r') as f:
+                metadata = json.load(f)
+            # Get last updated date from metadata
+            lastUpdated = metadata['lastUpdated']
+        else:
+            # Get the last updated date from the existing files
+            lastUpdated  = self.getLastUpdatedFromItemFiles(self.directory)
+            if lastUpdated:
+                # Save the last updated date in a metadata file
+                with open(metadataFilename, 'w') as f:
+                    json.dump({'lastUpdated': lastUpdated}, f)
+        return lastUpdated
+    
+    def getLastUpdatedFromItemFiles(self):
+        # Read all XML files in the input folder
+        files = [f for f in listdir(self.directory) if isfile(join(self.directory, f)) and f.endswith('.xml')]
+        
+        # If no files exist yet, return None
+        if len(files) == 0:
+            return None
+
+        # Set lastUpdated to a Date object with the lowest possible value
+        lastUpdated = datetime.min
+
+        for file in files:
+            tree = etree.parse(join(self.directory, file))
+            lastUpdatedString= tree.find('.//{http://www.zetcom.com/ria/ws/module}systemField[@name="__lastModified"]/{http://www.zetcom.com/ria/ws/module}value').text
+            lastUpdatedItem = datetime.strptime(lastUpdatedString, '%Y-%m-%d %H:%M:%S.%f')
+            if lastUpdatedItem > lastUpdated:
+                lastUpdated = lastUpdatedItem
+        return lastUpdated.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+
+    def setLastUpdated(self, lastUpdated):
+        metadataFilename = join(self.directory, self.METADATA_FILENAME)
+        with open(metadataFilename, 'w') as f:
+            json.dump({'lastUpdated': lastUpdated.strftime('%Y-%m-%dT%H:%M:%S.%f')}, f)
