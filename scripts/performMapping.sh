9e4f922bd190d6ea7c985a731a111d818561cf5f
jbatin <josip.batin@gmail.com>
2024-12-10 16:44:51 +0100

    implemented java mapping service

--- scripts/performMapping.sh
+++ scripts/performMapping.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+# !/bin/bash
 
-usage() { echo "Usage: $0 [-i <input folder>] [-o <output folder>] [-m <mapping file>] [-g <generator policy>] [-b <batch size>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-i <input folder>] [-o <output folder>] [-m <mapping file>] [-g <generator policy>] [-b <batch size>] [-j <java transform service endopoint>]" 1>&2; exit 1; }
 
-while getopts ":i:o:m:g:b:" o; do
+while getopts ":i:o:m:g:b:j:" o; do
     case "${o}" in
         i)
             RECORDSINPUTFOLDER=${OPTARG}
@@ -19,6 +19,9 @@ while getopts ":i:o:m:g:b:" o; do
         b)
             BATCHSIZE=${OPTARG}
             ;;
+        j)
+            SERVER_URL=${OPTARG}
+            ;;
         *)
             usage
             ;;
@@ -26,27 +29,48 @@ while getopts ":i:o:m:g:b:" o; do
 done
 shift $((OPTIND-1))
 
-echo "Mapping Records"
-numfiles=$(find $RECORDSINPUTFOLDER -type f -name '*.xml' | wc -l)
-count=1
-echo "Found $numfiles record XML files in $RECORDSINPUTFOLDER"
+process_file() {
+    local file="$1"
+    local output_file="$RECORDSOUTPUTFOLDER/$(basename "$file" .xml).ttl"
+    echo
+    echo curl -X POST "$SERVER_URL?mappingFile=$RECORDMAPPING&generatorPolicy=$GENERATOR&inputFile=$file&outputFile=$output_file"
+    curl -X POST "$SERVER_URL?mappingFile=$RECORDMAPPING&generatorPolicy=$GENERATOR&inputFile=$file&outputFile=$output_file"
+    echo
+}
 
-(
-for f in $(find $RECORDSINPUTFOLDER -type f -name '*.xml' ); do
-  ((i=i%BATCHSIZE)); ((i++==0)) && wait
-  echo "Mapping record $count of $numfiles ($f)"
-  o=${f/.xml/.ttl}
-  o=${o/$RECORDSINPUTFOLDER/}
-  java --add-opens java.base/java.lang.reflect=ALL-UNNAMED \
-    --add-opens java.base/java.util=ALL-UNNAMED \
-    --add-opens java.base/java.text=ALL-UNNAMED \
-    --add-opens java.desktop/java.awt.font=ALL-UNNAMED \
-    -jar /x3ml/x3ml-engine.exejar \
-    --input $f \
-    --x3ml $RECORDMAPPING \
-    --policy $GENERATOR \
-    --output $RECORDSOUTPUTFOLDER/$o \
-    --format text/turtle &
-  count=$((count+1)) 
+export -f process_file
+export SERVER_URL
+export RECORDSOUTPUTFOLDER
+export RECORDMAPPING
+export GENERATOR
+
+# process a batch of files
+process_batch() {
+    batch=("$@")
+    echo "Processing batch of ${#batch[@]} files:"
+    for file in "${batch[@]}"; do
+        process_file "$file"
+    done
+}
+
+batch=()
+
+for file in "$RECORDSINPUTFOLDER"/*.xml; do
+    if [ -f "$file" ]; then
+        batch+=("$file")
+
+        # Process the batch if we reach the batch size
+        if [ "${#batch[@]}" -eq "$BATCHSIZE" ]; then
+            process_batch "${batch[@]}"
+            batch=() # Reset the batch
+        fi
+    else
+        echo "No XML files found in the directory."
+        break
+    fi
 done
-)
+
+# Process any remaining files in the batch
+if [ "${#batch[@]}" -gt 0 ]; then
+    process_batch "${batch[@]}"
+fi
