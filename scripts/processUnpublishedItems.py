a8b02ae4789bd0d6af3e7b214e883a2df0e74199
Florian Kräutli <florian.kraeutli@uzh.ch>
2026-01-07 13:54:42 +0100

    feat: enhance output message with total items count in synchroniseItems function

--- scripts/processUnpublishedItems.py
+++ scripts/processUnpublishedItems.py
@@ -41,7 +41,7 @@ def synchroniseItems(*, host, username, password, module, inputFolder, turtleFol
     totalItemsInModule = client.getNumberOfItems(module=module, queryAddition=createXMLCopy(queryAddition))
     
     if totalItemsInModule == len(files):
-        print("Total number of items matches the number of local files. Would you like to check each item individually for unpublished status? (y/N): ", end='', flush=True)
+        print(f"Total number of items ({totalItemsInModule}) matches the number of local files. Would you like to check each item individually for unpublished status? (y/N): ", end='', flush=True)
         choice = 'n'
         rlist, _, _ = select.select([sys.stdin], [], [], 5)
         if rlist:
